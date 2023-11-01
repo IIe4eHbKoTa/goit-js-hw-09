20e62e46c916912aaf6a332ffefb6dd4c7f70e8d
IIe4eHbKoTa <136131741+IIe4eHbKoTa@users.noreply.github.com>
2023-11-01 09:18:50 +0000

    Deploying to gh-pages from @ IIe4eHbKoTa/goit-js-hw-09@68c86162b2e9c90efc5308f19fb086d1654b6b2c 🚀

---
+++ 01-color-switcher.d0868d3d.js
@@ -0,0 +1,2 @@
+let e=null;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=e}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.d0868d3d.js.map
